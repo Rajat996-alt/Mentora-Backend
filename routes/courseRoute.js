c862750242c261dcae6e9a5152c46496398ecf64
Rajat Sen <rajatsen7568@gmail.com>
2025-11-23 11:59:37 +0530

    Added AI-Search functionality

--- routes/courseRoute.js
+++ routes/courseRoute.js
@@ -9,9 +9,11 @@ const {
 } = require("../controllers/courseController");
 const userAuth = require("../middlewares/userAuth");
 const upload = require("../middlewares/multer");
+const { searchWithAI } = require("../controllers/searchController");
 
 const courseRouter = express.Router();
 
+courseRouter.post("/search", searchWithAI);
 courseRouter.post("/create", userAuth, createCourse);
 courseRouter.get("/published", getPublishedCourses);
 courseRouter.get("/creator", userAuth, getCreatorCourses);
